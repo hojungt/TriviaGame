3604920c2a433e8e4a38cb7dd52fcc409fa56af5
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-02-13 22:59:23 -0500

    initial setup, pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+
+
+
+// Variables - set game score as state object:
+
+var gameState = {
+    rightCount: 0,
+    wrongCount: 0,
+    unansweredCount: 0,
+};
+
+// Variables - set question-answer set as object:
+
+var questionSet = {
+    Set1: {Q, A1, A2, A3, A4},
+    Set2: {Q, A1, A2, A3, A4},
+    Set3: {Q, A1, A2, A3, A4},
+    Set4: {Q, A1, A2, A3, A4},
+    Set5: {Q, A1, A2, A3, A4},
+    Set6: {Q, A1, A2, A3, A4},
+    Set7: {Q, A1, A2, A3, A4},
+    Set8: {Q, A1, A2, A3, A4},
+    Set9: {Q, A1, A2, A3, A4},
+    Set10: {Q, A1, A2, A3, A4},
+};
+
+// Link values to HTML elements:
+
+
+
+// For Loop - append questionSet with class and button attribute:
+
+
